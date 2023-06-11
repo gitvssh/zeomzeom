60448b3b9860a527f0657eda14e19e9e244e8cf0
gitvssh <39320478+gitvssh@users.noreply.github.com>
2023-06-12 00:17:29 +0900

    :construction: update Controller

--- src/main/java/com/lowwall/zeomzeom/controller/AnswerController.java
+++ src/main/java/com/lowwall/zeomzeom/controller/AnswerController.java
@@ -1,12 +1,12 @@
 package com.lowwall.zeomzeom.controller;
 
+import com.lowwall.zeomzeom.controller.dto.AnswerRequestDTO;
+import com.lowwall.zeomzeom.controller.dto.AnswerResponseDTO;
+import com.lowwall.zeomzeom.entity.Answer;
 import com.lowwall.zeomzeom.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/answer")
@@ -17,25 +17,46 @@ public class AnswerController {
 
     @PostMapping("/{answerId}")
     //@Operation(summary = "답변 등록", description = "2-2-2 오늘의 질문, 답변 등록")
-    public String answer() {
-        return "answer/answer";
+    public AnswerResponseDTO answer(@PathVariable Long answerId, AnswerRequestDTO answerRequestDTO) {
+        //답변 조회, 존재여부 확인, 답변 등록 여부확인(x)
+        Answer one = answerService.findOne(answerId);
+        //답변 등록
+        one.setAnswerContent(answerRequestDTO.getAnswerText());
+
+        answerService.updateAnswer(answerId, one);
+
+        AnswerResponseDTO answerResponseDTO = new AnswerResponseDTO();
+        answerResponseDTO.setAnswerId(one.getId());
+        answerResponseDTO.setPairQuestionId(answerRequestDTO.getPairQuestionId());
+        answerResponseDTO.setAnswerContent(one.getAnswerContent());
+        answerResponseDTO.setCreatedAt(one.getCreatedDate());
+        answerResponseDTO.setUpdatedAt(one.getLastModifiedDate());
+        //답변 결과 조회 반환
+        return answerResponseDTO;
     }
 
     @PutMapping("/{answerId}")
     //@Operation(summary = "답변 수정", description = "답변 수정")
     public String update() {
+        //답변 조회, 존재여부 확인, 답변 등록 여부 확인(o)
+        //답변 수정
+        //답변 수정 결과 조회 반환
         return "answer/update";
     }
 
     @GetMapping("/{answerId}")
     //@Operation(summary = "답변 삭제", description = "2-3-1 오늘의 질문 답변 후\\n\" +\n2-3-1과 2-3-2의 차이는 답변조회 결과수로 구별")
     public String detail() {
+        //답변 조회, 존재여부 확인
+        //답변 삭제
+        //답변 삭제 결과 조회 반환
         return "answer/detail";
     }
 
     @GetMapping("/")
     //@Operation(summary = "답변 목록 조회", description = "3 날짜별 보관함, 카테고리별 ")
     public String list() {
+        //답변 전체 조회 반환
         return "answer/list";
     }
 }
--- src/main/java/com/lowwall/zeomzeom/controller/QuestionController.java
+++ src/main/java/com/lowwall/zeomzeom/controller/QuestionController.java
@@ -1,5 +1,10 @@
 package com.lowwall.zeomzeom.controller;
 
+import com.lowwall.zeomzeom.controller.dto.QuestionResponseDTO;
+import com.lowwall.zeomzeom.entity.Pair;
+import com.lowwall.zeomzeom.entity.PairQuestion;
+import com.lowwall.zeomzeom.entity.Question;
+import com.lowwall.zeomzeom.service.PairQuestionService;
 import com.lowwall.zeomzeom.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,22 +12,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/api/question")
 @RequiredArgsConstructor
 //@Tag(name = "5. Question", description = "Question API")
 public class QuestionController {
     private final QuestionService questionService;
+    private final PairQuestionService pairQuestionService;
 
     @PostMapping("/")
     //@Operation(summary = "질문 생성 요청", description = "2-1 오늘의 질문, 요청 시 질문 생성")
-    public String question() {
-        return "question/question";
+    public QuestionResponseDTO question() {
+        //페어 질문 생성
+        Long pairQuestionId = questionService.create();
+        PairQuestion pairQuestion = pairQuestionService.findById(pairQuestionId);
+        Pair pair = pairQuestion.getPair();
+        Question question = pairQuestion.getQuestion();
+
+        //페어 질문 반환
+        QuestionResponseDTO questionResponseDTO = new QuestionResponseDTO();
+        questionResponseDTO.setPairQuestionId(pairQuestionId);
+        questionResponseDTO.setPairId(pair.getId());
+        questionResponseDTO.setQuestionId(question.getId());
+        questionResponseDTO.setQuestion(question.getQuestion());
+        questionResponseDTO.setClosingMessage(question.getClosingMessage());
+
+        return questionResponseDTO;
     }
 
     @GetMapping("/")
     //@Operation(summary = "질문 목록 조회", description = "2-1 오늘의 질문, 질문 목록 조회")
-    public String list() {
-        return "question/list";
+    public List<Question> list() {
+        //페어 질문 목록 조회
+        //TODO: 페어 엔티티 DTO로 매핑하여 반환
+        return questionService.findAll();
     }
 }
